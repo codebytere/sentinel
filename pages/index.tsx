f551ea931f2576a542a8677d3810d284d25aca98
Shelley Vohr <shelley.vohr@gmail.com>
2020-06-02 12:26:38 -0700

    feat: reorganize and add chart to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,16 @@
 import { Component } from 'react'
 
-import { Table, Hero, Container, Columns } from 'react-bulma-components'
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer
+} from 'recharts'
+import { Box, Columns, Container, Hero, Table } from 'react-bulma-components'
 import { api } from '../src/server/api'
 import { IRequest } from 'src/server/interfaces'
 
@@ -40,7 +50,10 @@ class Home extends Component<{ requests: IRequest[] }, {}> {
       <Hero color={'info'} size={'fullheight'}>
         <Hero.Body>
           <Container>
-            <Columns className={'is-centered'}>
+            <Columns centered>
+              <Columns.Column>{this.renderTrendChart()}</Columns.Column>
+            </Columns>
+            <Columns centered>
               <Columns.Column>{this.renderRequests()}</Columns.Column>
             </Columns>
           </Container>
@@ -51,25 +64,111 @@ class Home extends Component<{ requests: IRequest[] }, {}> {
 
   /* PRIVATE METHODS */
 
+  private getStatusIcon(passed, total) {
+    let statusIcon
+    if (total === 0) {
+      statusIcon = '🟡'
+    } else if (passed === total) {
+      statusIcon = '🟢'
+    } else {
+      statusIcon = '🔴'
+    }
+
+    return statusIcon
+  }
+
+  private getReportStats (request: IRequest) {
+    return { 
+      total: request.reports.length,
+      passed: request.reports.filter(
+        rep => rep.table.status === api.Status.PASSED
+      ).length
+    }
+  }
+
+  private renderTrendChart() {
+    const { requests } = this.props
+
+    const data = requests.map(r => {
+      const { passed, total } = this.getReportStats(r)
+
+      const percentage = total === 0 ? total : (passed / total) * 100
+
+      const date = new Date(r.table.createdAt)
+      return {
+        date: date.toLocaleDateString(),
+        passed,
+        total,
+        percentage
+      }
+    })
+
+    const CustomTooltip = (tooltipData) => {
+      const { active, payload, label } = tooltipData
+
+      const style = {
+        'background': 'white',
+        'border': '2px solid black',
+        'padding': '10px',
+        'border-radius': '10px'
+      }
+
+      if (active) {
+        const data = payload[0].payload
+        return (
+          <div style={style}>
+            <p className="label">{label}</p>
+            <p>Reports Passed: {data.passed}</p>
+            <p>Total Reports: {data.total}</p>
+          </div>
+        );
+      }
+    
+      return null;
+    };
+
+    return (
+      <Box>
+        <ResponsiveContainer minHeight={'50vh'} width={'95%'}>
+          <LineChart
+            data={data}
+            margin={{ top: 20, right: 10, left: 0, bottom: 5 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" />
+            <YAxis domain={[0, 100]} tickFormatter={(number) => `${number}%`}/>
+            <Tooltip content={<CustomTooltip/>}/>
+            <Legend />
+            <Line
+              type="monotone"
+              dataKey="percentage"
+              stroke=" #000000"
+              activeDot={{ r: 8 }}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </Box>
+    )
+  }
+
   private renderRequests() {
-    const requestData = this.props.requests.map(r => {
+    const { requests } = this.props
+    const requestData = requests.map(r => {
       const { versionQualifier, id } = r.table
       const releaseLink = `https://github.com/electron/nightlies/releases/tag/v${versionQualifier}`
 
-      const numReports = r.reports.length
-      const numPassed = r.reports.filter(
-        rep => rep.table.status === api.Status.PASSED
-      ).length
+      const { passed, total } = this.getReportStats(r)
 
       return (
         <tr>
+          <th>{this.getStatusIcon(passed, total)}</th>
+          <th>{`${passed}/${total}`}</th>
           <td>v{versionQualifier}</td>
-          <td>{`${numPassed}/${numReports}`}</td>
           <td>
             <a href={releaseLink}>GitHub Release</a>
           </td>
           <td>
-            {numReports > 0 ? (
+            {total > 0 ? (
               <a href={`/request/${id}`}>See Reports</a>
             ) : (
               'No Reports'
@@ -80,18 +179,20 @@ class Home extends Component<{ requests: IRequest[] }, {}> {
     })
 
     return (
-      // TODO(codebytere): style this
-      <Table bordered id={'nightlies-table'}>
-        <tbody>
-          <tr>
-            <th>Version</th>
-            <th>Status</th>
-            <th>Release</th>
-            <th>Reports</th>
-          </tr>
-          {requestData}
-        </tbody>
-      </Table>
+      <Box>
+        <Table bordered id={'nightlies-table'}>
+          <tbody>
+            <tr>
+              <th>Status</th>
+              <th>Report Count</th>
+              <th>Version</th>
+              <th>Release</th>
+              <th>Reports</th>
+            </tr>
+            {requestData}
+          </tbody>
+        </Table>
+      </Box>
     )
   }
 }
